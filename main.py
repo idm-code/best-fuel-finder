9341766de6eb8b09981d711edb74d6a9f1017684
idm-code <ivandm.code@gmail.com>
2025-01-01 15:40:52 +0100

    Remove unnecessary timing code and improve province filter logic in gas price retrieval

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import requests
-import time
 
 # Configuración de la página
 st.set_page_config(page_title="Best Fuel Finder", page_icon="⛽", layout="wide")
@@ -10,7 +9,6 @@ st.set_page_config(page_title="Best Fuel Finder", page_icon="⛽", layout="wide"
 def get_gas_prices():
     url = "https://sedeaplicaciones.minetur.gob.es/ServiciosRESTCarburantes/PreciosCarburantes/EstacionesTerrestres/"
     try:
-        start_time = time.time()
         response = requests.get(url, timeout=10)
         response.raise_for_status()
         data = response.json()
@@ -30,8 +28,6 @@ def get_gas_prices():
             if est['Precio Gasolina 95 E5'] and est['Precio Gasoleo A']
         ]
         df = pd.DataFrame(datos)
-        end_time = time.time()
-        print(f"Tiempo de carga: {end_time - start_time:.2f} segundos")
         return df
     except requests.exceptions.RequestException as e:
         st.error(f"Error en la solicitud de la API: {e}")
@@ -53,10 +49,10 @@ def main():
     st.markdown("<h1 style='text-align: center;'>Precios de los carburantes en España</h1>", unsafe_allow_html=True)
 
     # Filtro por provincia
-    provincias = ["Todas"] + sorted(df['Provincia'].unique())
+    provincias = [""] + sorted(df['Provincia'].unique())
     provincia_seleccionada = st.selectbox("Seleccionar provincia:", options=provincias)
 
-    if provincia_seleccionada != "Todas":
+    if provincia_seleccionada != "":
         df = df[df['Provincia'] == provincia_seleccionada]
 
     # Mostrar una muestra inicial para reducir el tiempo de renderizado
@@ -64,14 +60,8 @@ def main():
     if not mostrar_todo:
         df = df.head(500)  # Ajusta este número según tus necesidades
 
-    # Tiempo de renderizado
-    render_start = time.time()
-
     # Mostrar el DataFrame interactivo
     st.dataframe(df, use_container_width=True)
 
-    render_end = time.time()
-    st.write(f"Tiempo de renderizado: {render_end - render_start:.2f} segundos")
-
 if __name__ == "__main__":
-        main()
+    main()
